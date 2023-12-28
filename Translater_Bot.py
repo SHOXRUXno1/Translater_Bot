0765a77e0f57337c74cedcae65928017c151ea38
SHOXRUXno1 <brawlstars7471255@gmail.com>
2023-12-28 23:45:17 +0500

    Translater_Bot

---
+++ Translater_Bot.py
@@ -0,0 +1,12 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import Command
+from googletrans import Translator
+from loader import dp
+
+tarjima=Translator()
+@dp.message_handler(Command("translater"))
+async def bot_trans(message: types.Message):
+    matn = message.text
+    tarjimon = tarjima.translate(matn,dest='en')
+    tarjima_qilindi = tarjimon.text
+    await message.reply(tarjima_qilindi)
